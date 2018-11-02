692485a2e1fb974bf02ff460f5035b67fd5dff06
Nathan Black <2210262@flourbluffschools.org>
2018-11-02 13:22:22 -0500

    methods written

--- textreader.py
+++ textreader.py
@@ -15,7 +15,7 @@ def at_least():
                     print(word)
                     
 def has_no_e(word):
-    if 'e' in word:
+    if 'e' in word or 'E' in word:
         return False
     return True
 
@@ -33,24 +33,75 @@ def no_e():
         
 def avoids(word, forbidden):
     for i in forbidden:
-        if i in word:
+        if i.lower() in word or i.upper() in word:
             return False
     return True
 
 def count_avoids():
     count = 0
-    subCount = 0
     forbidden = str(input("Enter a string of characters you want to avoid: "))
     with open("words.txt") as file:
         for line in file:
             for word in line.split():
-                for i in range(len(forbidden)):
-                    if forbidden[i] not in word:
-                        subCount += 1
-                    if subCount == len(forbidden):
-                        count += 1
-                subCount = 0
+                if avoids(word, forbidden):
+                    count += 1
         print(count)
+        
+def uses_only(word, usesString):
+    for i in usesString:
+        if i.lower() not in word or i.upper() not in word:
+            return False
+    return True
+
+def words_with_only():
+    count = 0
+    usesString = str(input("Enter a string of characters you want the word to have: "))
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.split():
+                if uses_only(word, usesString):
+                    count += 1
+        print(count)
+        
+def uses_all(word, usesString):
+    count = 0
+    for i in usesString:
+        if i in word:
+            count += 1
+            if count == len(usesString):
+                return True
+    return False
+
+def how_many_uses_all(usesString):
+    count = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.split():
+                if uses_all(word, usesString):
+                    count += 1
+        print(count)
+        
+        
+def is_abecedarian(word):
+    for i in range(1,len(word)):
+        if word[i] < word[i-1]:
+            return False
+    return True
+
+def count_abecdarian():
+    count = 0
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.split():
+                if is_abecdarian(word):
+                    count += 1
+        print(count)
+        
+        
+    
+            
+
+    
                         
                 
     
