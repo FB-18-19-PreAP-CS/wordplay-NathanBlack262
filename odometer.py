1844cc814ace75bde055b79563c27409dc8086f7
Nathan Black <nathanblacknotblock262@gmail.com>
2018-11-06 05:52:07 -0600

    not fully working, will fix in class

---
+++ odometer.py
@@ -0,0 +1,13 @@
+
+
+for i in range(100000, 999999):
+    j = i
+    eyy = str(j)
+    if eyy[2:6] == eyy[5:1:-1]:
+        eyyMod = str(j+1)
+        if eyyMod[1:6] == eyyMod[5:0:-1]:
+            eyyMod = str(j+1)
+            if eyyMod[1:5] == eyyMod[4:0:-1]:
+                eyyMod = str(j+1)
+                if eyyMod[0:6] == eyyMod[5:-1:-1]:
+                    print(eyy[0] + "-" + eyy[1] + "-" + eyy[2] + "-" + eyy[3] + "-" + eyy[4] + "-" + eyy[5]) 
